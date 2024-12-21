e2749ebc8d091636c8815660442cc5e19ddddfbd
Gazouly <m.algazouly96@gmail.com>
2024-12-21 21:09:43 +0200

    chore(core): 💄 add shadcn support

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   server: {
     port: 4000,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer, tailwind()],
+    },
   },
   resolve: {
     alias: {
