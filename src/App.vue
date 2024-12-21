37c746f9bd8f37e9250050d24bb046b78519d0d8
Gazouly <m.algazouly96@gmail.com>
2024-12-21 21:22:24 +0200

    chore(ui): 💄 add base components needed for layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
+  <AppLayout />
   <RouterView />
 </template>
